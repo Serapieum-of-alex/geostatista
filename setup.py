13ad929dcb863142d96a04d421b48eaeba0e2333
Mostafa Farrag <moah.farag@gmail.com>
2023-01-31 20:14:01 +0100

    bump up versions

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements_dev = [line.strip() for line in open("requirements-dev.txt").readli
 
 setup(
     name="geostatista",
-    version="0.1.5",
+    version="0.1.6",
     description="statistics package",
     author="Mostafa Farrag",
     author_email="moah.farag@gmail.come",
